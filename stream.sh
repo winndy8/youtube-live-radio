1f8e6ec3ad96b3ea98430e4df59a738322d3522b
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-01 17:28:48 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -12,9 +12,8 @@ AUDIO_SOURCE="mp3/the-corner-in-the-town-121352-[AudioTrimmer.com].mp3"
 
 ffmpeg \
     -re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB)" \
-    -thread_queue_size 512 -i "$AUDIO_SOURCE" \
-    -map 0:v:0 -map 1:a:0 \
-    -map_metadata:g 1:g \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+      -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
+    -c:a aac -b:a 128k -ar 44100 \
     -f flv "$YOUTUBE_URL/$KEY"
