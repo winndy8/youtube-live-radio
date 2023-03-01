84c5717a46e23e8280126936e9962455cb8f847d
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-01 17:45:25 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -11,7 +11,7 @@ VIDEO_SOURCE="bg.mp4"
 AUDIO_SOURCE="mp3/the-corner-in-the-town-121352-[AudioTrimmer.com].mp3"
 
   ffmpeg -loglevel info -y -re \
-    -f image2 -loop 1 -i "VIDEO_SOURCE" \
+    -f image2 -loop 1 -i bg.mp4 \
     -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
