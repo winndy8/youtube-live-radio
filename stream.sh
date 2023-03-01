6c7cad409f54bbe0f6d008ad04a977cb7ce829d6
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-01 19:00:34 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -10,7 +10,7 @@ KEY="z5k3-8888-69qu-9999"
 SOURCE="video.mp4"
 
 ffmpeg \
-    -stream_loop -1 -i "$SOURCE" -deinterlace \
+    -i "$SOURCE" -deinterlace \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
     -f flv "$YOUTUBE_URL/$KEY"
