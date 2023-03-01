3b50fe57c582ca2944a332d4c37594c2a04f3987
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-01 16:16:53 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
+#! /bin/bash
 
-set -e
+VBR="1500k"
+FPS="24"
+QUAL="superfast"
 
-while true
-do
-  ffmpeg -loglevel info -y -re \
-    -f video2 -loop 1 -i bg.mp4 \
-    -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
-    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
-    -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
-done
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
+KEY="z5k3-8888-69qu-9999"
+
+VIDEO_SOURCE="bg.mp4"
+AUDIO_SOURCE="the-corner-in-the-town-121352-[AudioTrimmer.com].mp3"
+
+ffmpeg \
+    -re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB)" \
+    -thread_queue_size 512 -i "$AUDIO_SOURCE" \
+    -map 0:v:0 -map 1:a:0 \
+    -map_metadata:g 1:g \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY"
