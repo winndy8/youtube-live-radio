ec0e003b8dca6099d8297365dbdecbca871a517d
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-01 17:11:37 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -12,7 +12,7 @@ AUDIO_SOURCE="mp3/the-corner-in-the-town-121352-[AudioTrimmer.com].mp3"
 
 ffmpeg \
     -re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB)" \
-    -thread_queue_size 35 -i "$AUDIO_SOURCE:loop=0" \
+    -thread_queue_size 34 -i "$AUDIO_SOURCE:loop=0" \
     -map 0:v:0 -map 1:a:0 \
     -map_metadata:g 1:g \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
